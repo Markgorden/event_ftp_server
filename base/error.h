43e2a28c9a79e14f0a4d23a1b8075f2a9b19f6b2
vanbreaker <365201058@qq.com>
2016-02-29 14:30:54 +0800

    Update error.h
    tab to 4 space

--- base/error.h
+++ base/error.h
@@ -4,18 +4,18 @@
 enum common_error_code
 {
     SUCCESS = 0,
-	ERR_NOMEM,
-	ERR_INVALID_PARAM,
+    ERR_NOMEM,
+    ERR_INVALID_PARAM,
 };
 
 enum fs_error_code
 {
-	ERR_FS = 0x10,
-	ERR_FS_OPEN,
-	ERR_FS_READ,
-	ERR_FS_WRITE,
-	ERR_FS_STAT,
-	ERR_FS_RM,
+    ERR_FS = 0x10,
+    ERR_FS_OPEN,
+    ERR_FS_READ,
+    ERR_FS_WRITE,
+    ERR_FS_STAT,
+    ERR_FS_RM,
 };
 
 
