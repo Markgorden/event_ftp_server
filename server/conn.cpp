9e9a9f8ccf00263c312ba3282b0e2b471f4ba22b
vanbreaker <365201058@qq.com>
2016-02-29 14:00:18 +0800

    Update conn.cpp
    tab to 4 space

--- server/conn.cpp
+++ server/conn.cpp
@@ -8,30 +8,30 @@ Conn::Conn()
 
 Conn::~Conn()
 {
-	bufferevent_free(bev_);
+    bufferevent_free(bev_);
 }
 
 Libevent_thread *Conn::get_attached_worker()
 {
-	return attached_worker_;
+    return attached_worker_;
 }
 
 int Conn::get_socket_fd()
 {
-	return fd_;
+    return fd_;
 }
 
 void Conn::set_attached_worker(Libevent_thread * worker)
 {
-	attached_worker_ = worker;
+    attached_worker_ = worker;
 }
 
 struct bufferevent *Conn::get_bev()
 {
-	return bev_;
+    return bev_;
 }
 
 void Conn::set_bev(struct bufferevent *bev)
 {
-	bev_ = bev;
+    bev_ = bev;
 }
