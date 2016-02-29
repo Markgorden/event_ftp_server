36de409ad02c71e650293408200b758969b001a1
vanbreaker <365201058@qq.com>
2016-02-29 14:20:37 +0800

    Update event_server.cpp
    tab to 4 space

--- server/event_server.cpp
+++ server/event_server.cpp
@@ -10,12 +10,12 @@
 
 Event_server::Event_server(int port)
 {
-	Net_addr local_addr(port);
+    Net_addr local_addr(port);
 
     listener_.set_reuse_addr(true);
     listener_.bind_addr(local_addr);
-	socks::set_nonblock(listener_.get_sockfd());
-	listener_.listen();
+    socks::set_nonblock(listener_.get_sockfd());
+    listener_.listen();
 }
 
 
@@ -29,41 +29,40 @@ Event_server::~Event_server()
 
 void Event_server::run()
 {
+    assert((dispatcher_ = new Dispatcher(this)) != NULL);
 
-	assert((dispatcher_ = new Dispatcher(this)) != NULL);
+    assert((main_base_ = event_base_new()) != NULL);
+    assert(event_assign(&listen_event_, main_base_, listener_.get_sockfd(), EV_READ | EV_PERSIST,
+           Event_server::accept_event_cb, this) == 0);
+    assert(event_add(&listen_event_, NULL) == 0);
 
-	assert((main_base_ = event_base_new()) != NULL);
-	assert(event_assign(&listen_event_, main_base_, listener_.get_sockfd(), EV_READ | EV_PERSIST,
-				Event_server::accept_event_cb, this) == 0);
-	assert(event_add(&listen_event_, NULL) == 0);
-
-	event_base_dispatch(main_base_);
+    event_base_dispatch(main_base_);
 }
 
 
 
 void Event_server::accept_event_cb(evutil_socket_t listener, short event, void *arg)
 {
-	int conn_fd;
-	struct sockaddr_in addr;	
-	char buf[32];
-	Event_server *ser = static_cast<Event_server*>(arg);
-	Conn *connection;
+    int conn_fd;
+    struct sockaddr_in addr;	
+    char buf[32];
+    Event_server *ser = static_cast<Event_server*>(arg);
+    Conn *connection;
 	
-	conn_fd = socks::accept(listener, &addr);
-	if(conn_fd < 0)
-	{
-		return;
-	}
+    conn_fd = socks::accept(listener, &addr);
+    if(conn_fd < 0)
+    {
+        return;
+    }
 
-	socks::to_ip_port(buf, sizeof(buf), addr);
-	std::cout << "connect from " << buf <<std::endl;
+    socks::to_ip_port(buf, sizeof(buf), addr);
+    std::cout << "connect from " << buf <<std::endl;
 		
     connection = ser->handle_accept_event(conn_fd);
 
-	connection->fd_ = conn_fd;
+    connection->fd_ = conn_fd;
 
-	printf("socket-->%d, c-->%p\n",connection->get_socket_fd(), connection);
+    printf("socket-->%d, c-->%p\n",connection->get_socket_fd(), connection);
 
     ser->dispatcher_->dispatch(connection);
 }
@@ -72,12 +71,12 @@ void Event_server::accept_event_cb(evutil_socket_t listener, short event, void *
 
 void Event_server::read_event_cb(struct bufferevent *bev, void *arg)
 {
-     Conn *connection = static_cast<Conn *>(arg);
-     Event_server *ser = connection->get_attached_worker()->get_event_server();
+    Conn *connection = static_cast<Conn *>(arg);
+    Event_server *ser = connection->get_attached_worker()->get_event_server();
 
-	 std::cout << "read event" << std::endl;
+    std::cout << "read event" << std::endl;
 
-     ser->handle_read_event(connection);
+    ser->handle_read_event(connection);
 }
 
 
@@ -87,7 +86,7 @@ void Event_server::write_event_cb(struct bufferevent *bev, void *arg)
     Conn *connection = static_cast<Conn *>(arg);
     Event_server *ser = connection->get_attached_worker()->get_event_server();
 
-	 std::cout << "write event" << std::endl;
+    std::cout << "write event" << std::endl;
 
     ser->handle_write_event(connection);
 }
